97dcc16d965092be0d692699b17548bf6d692a76
Muhammad Tariq <f158185@nu.edu.pk>
2020-03-15 19:32:44 +0500

    sign, sign up forgot password ui added

---
+++ app/src/main/java/com/figuredin/controllers/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.figuredin.controllers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Thread{
+            Thread.sleep(3000)
+        }.start()
+
+
+    }
+
+}
